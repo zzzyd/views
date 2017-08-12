55346286760409ab2badd261004bb0be61017adb
aa <aa@aa.com>
2017-08-12 16:52:53 +0800

    初始化博学谷和设置了登录的跳转页面

---
+++ views/public/js/common.js
@@ -0,0 +1,12 @@
+
+	NProgress.start();
+
+	NProgress.done();
+
+	$('.navs ul').prev('a').on('click', function () {
+		$(this).next().slideToggle();
+	});
+
+	if(!$.cookie('PHPSESSID')&&location.pathname!='/login'){
+			window.location.href = '/login';
+	}
